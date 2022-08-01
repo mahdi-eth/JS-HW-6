4b79d433b9c51363fa7c376d35b31119897d5d8c
Mahdi EttehadNejad <99660553+mahdi-Eth@users.noreply.github.com>
2022-08-01 21:48:39 +0430

    Add files via upload

--- JS-HW-6/index.js
+++ JS-HW-6/index.js
@@ -1,10 +1,9 @@
-export const submit = document.getElementById("submit");
+const submit = document.getElementById("submit");
 
 const userDataStorage = JSON.parse(localStorage.getItem("data")) || [];
-export let userLocalStorage = [...userDataStorage];
-console.log(userLocalStorage);
+let userLocalStorage = [...userDataStorage];
 
-export const createDets = (event) => {
+const createDets = (event) => {
   event.preventDefault();
   const userData = {
     title: document.getElementById("title").value,
@@ -12,30 +11,144 @@ export const createDets = (event) => {
     Description: document.getElementById("Description").value,
     image: document.getElementById("image").value,
   };
-  console.log(userData);
+
+  userLocalStorage = [];
   userLocalStorage.push(userData);
   localStorage.setItem("data", JSON.stringify(userLocalStorage));
-  console.log(localStorage.getItem("data"));
+  location.reload();
 };
-if (userLocalStorage !== null) {
-userLocalStorage.forEach((item) => {
-  const formDataHtmlLi = document.createElement("li");
-  formDataHtmlLi.className = "formDataHtmlLiclass";
-  const formDataHtmlH2 = document.createElement("h2");
-  formDataHtmlH2.innerHTML = item.title;
-  const formDataHtmlH3 = document.createElement("h3");
-  formDataHtmlH3.innerHTML = item.creator;
-  const formDataHtmlDescription = document.createElement("p");
-  formDataHtmlDescription.innerHTML = item.Description;
-  const formDataHtmlimage = document.createElement("img");
-  formDataHtmlimage.setAttribute("src", item.image
-);
 
-  formDataHtmlLi.appendChild(formDataHtmlH2);
-  formDataHtmlLi.appendChild(formDataHtmlH3);
-  formDataHtmlLi.appendChild(formDataHtmlDescription);
-  formDataHtmlLi.appendChild(formDataHtmlimage);
-  document.body.appendChild(formDataHtmlLi);
-})};
+if (userLocalStorage.length > 0) {
+  if (
+    userLocalStorage[0].title == "" &&
+    userLocalStorage[0].Description == "" &&
+    userLocalStorage[0].creator == ""
+  ) {
+    alert("Please fill out all the information");
+  };
+
+  if (
+    userLocalStorage[0].title !== "" ||
+    userLocalStorage[0].Description !== "" ||
+    userLocalStorage[0].creator !== ""
+  ) {
+    userLocalStorage.forEach((item) => {
+      const main = document.createElement("ul");
+
+      const formDataHtmlLi = document.createElement("li");
+      formDataHtmlLi.className = "formDataHtmlLiclass";
+
+      const formDataHtmlH2 = document.createElement("h2");
+      formDataHtmlH2.innerHTML = item.title;
+
+      const formDataHtmlThumbsUp = document.createElement("i");
+
+      if (item.title) {
+        formDataHtmlThumbsUp.className =
+          localStorage.getItem("formDataHtmlThumbsUp") || "bi bi-heart";
+        formDataHtmlThumbsUp.addEventListener("click", () => {
+          if (formDataHtmlThumbsUp.className == "bi bi-heart") {
+            formDataHtmlThumbsUp.className = "bi bi-heart-fill";
+            localStorage.setItem(
+              "formDataHtmlThumbsUp",
+              formDataHtmlThumbsUp.className
+            );
+          } else {
+            formDataHtmlThumbsUp.className = "bi bi-heart";
+            localStorage.setItem(
+              "formDataHtmlThumbsUp",
+              formDataHtmlThumbsUp.className
+            );
+          }
+        });
+      }
+
+      const formDataHtmlThumbsUp2 = document.createElement("i");
+
+      if (item.creator) {
+        formDataHtmlThumbsUp2.className =
+          localStorage.getItem("formDataHtmlThumbsUp2") || "bi bi-heart";
+        formDataHtmlThumbsUp2.addEventListener("click", () => {
+          if (formDataHtmlThumbsUp2.className == "bi bi-heart") {
+            formDataHtmlThumbsUp2.className = "bi bi-heart-fill";
+            localStorage.setItem(
+              "formDataHtmlThumbsUp2",
+              formDataHtmlThumbsUp2.className
+            );
+          } else {
+            formDataHtmlThumbsUp2.className = "bi bi-heart";
+            localStorage.setItem(
+              "formDataHtmlThumbsUp2",
+              formDataHtmlThumbsUp2.className
+            );
+          }
+        });
+      }
+
+      const formDataHtmlThumbsUp3 = document.createElement("i");
+
+      if (item.Description) {
+        formDataHtmlThumbsUp3.className =
+          localStorage.getItem("formDataHtmlThumbsUp3") || "bi bi-heart";
+        formDataHtmlThumbsUp3.addEventListener("click", () => {
+          if (formDataHtmlThumbsUp3.className == "bi bi-heart") {
+            formDataHtmlThumbsUp3.className = "bi bi-heart-fill";
+            localStorage.setItem(
+              "formDataHtmlThumbsUp3",
+              formDataHtmlThumbsUp3.className
+            );
+          } else {
+            formDataHtmlThumbsUp3.className = "bi bi-heart";
+            localStorage.setItem(
+              "formDataHtmlThumbsUp3",
+              formDataHtmlThumbsUp3.className
+            );
+          }
+        });
+      }
+
+      const formDataHtmlThumbsUp4 = document.createElement("i");
+
+      if (item.title) {
+        formDataHtmlThumbsUp4.className =
+          localStorage.getItem("formDataHtmlThumbsUp4") || "bi bi-heart";
+        formDataHtmlThumbsUp4.addEventListener("click", () => {
+          if (formDataHtmlThumbsUp4.className == "bi bi-heart") {
+            formDataHtmlThumbsUp4.className = "bi bi-heart-fill";
+            localStorage.setItem(
+              "formDataHtmlThumbsUp4",
+              formDataHtmlThumbsUp4.className
+            );
+          } else {
+            formDataHtmlThumbsUp4.className = "bi bi-heart";
+            localStorage.setItem(
+              "formDataHtmlThumbsUp4",
+              formDataHtmlThumbsUp4.className
+            );
+          }
+        });
+      }
+
+      const formDataHtmlH3 = document.createElement("h3");
+      formDataHtmlH3.innerHTML = item.creator;
+      const formDataHtmlDescription = document.createElement("p");
+      formDataHtmlDescription.innerHTML = item.Description;
+      const formDataHtmlimage = document.createElement("div");
+      formDataHtmlimage.className = "ul-image";
+
+      formDataHtmlLi.appendChild(formDataHtmlH2);
+      formDataHtmlLi.appendChild(formDataHtmlThumbsUp);
+      formDataHtmlLi.appendChild(formDataHtmlH3);
+      formDataHtmlLi.appendChild(formDataHtmlThumbsUp2);
+      formDataHtmlLi.appendChild(formDataHtmlDescription);
+      formDataHtmlLi.appendChild(formDataHtmlThumbsUp3);
+      formDataHtmlLi.appendChild(formDataHtmlimage);
+      formDataHtmlLi.appendChild(formDataHtmlThumbsUp4);
+      main.appendChild(formDataHtmlLi);
+      document.body.appendChild(main);
+      submit.value = "Edit";
+    });
+  }
+}
 
 submit.addEventListener("click", createDets);
